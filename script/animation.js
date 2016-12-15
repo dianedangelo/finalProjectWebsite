e5b53afe821aed0a74c14650c3bc2ab4660a3681
Diane D'Angelo <ddangelo@nyc.rr.com>
2016-12-14 21:04:32 -0500

    my website so far

---
+++ script/animation.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  var buttons = $(".picHolder");	
+  $(buttons).on("click", function() {
+
+    if($(this).hasClass("small")) {
+      $(this).animate({
+        "width": "545px"
+      },1000);
+      $(this).find(".picHolderBlurb").fadeIn(2000);
+      $(this).addClass("expanded");
+      $(this).removeClass("small")
+  	}
+	else if($(this).hasClass("expanded")) {
+	  $(this).animate({
+	// "top":"20px",
+        "width": "200px"
+      },1000);
+	  $(this).find(".picHolderBlurb").hide("slow");
+	  $(this).addClass("small");
+	  $(this).removeClass("expanded")
+	}
+  })
+});
